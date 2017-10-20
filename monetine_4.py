adf51acdb582a64dc9fab3c5cea71e9c7493ccfa
XXX <nidadoni@gmail.com>
2017-10-20 11:33:37 +0200

    Primo commit - monetine non funzionante

---
+++ monetine_4.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 11:23:44 2017
+
+@author: cl460156
+"""
+
+def monete(x, L):
+    if x == 0:
+        return 1
+    if len(L) == 1:
+        if x % L[0]:
+            return 1
+        return 0
+    if L[0] > x:
+        return monete(x, L[1:])
+    return monete(x-L[0], L) + monete(x, L[1:])
+
+print (monete(20, [10,5,2,1]))
